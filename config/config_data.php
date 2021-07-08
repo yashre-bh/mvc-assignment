4d5536e70c1d87b7211314e7ed0754ed1622a105
Shreya Bhagat <shreyabhagat2002@gmail.com>
2021-07-08 21:01:58 +0530

    removed: config_data credentials

--- config/config_data.php
+++ config/config_data.php
@@ -3,5 +3,5 @@
 $DB_HOST ="127.0.0.1";
 $DB_PORT ="3306";
 $DB_NAME ="mvc-db";
-$DB_USERNAME ="root"; //enter username here
-$DB_PASSWORD ="Dobby@123"; //enter password here
+$DB_USERNAME =""; //enter mysql username
+$DB_PASSWORD =""; //enter password 
